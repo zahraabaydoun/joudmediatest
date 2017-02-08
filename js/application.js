24a09e2da9a19bd38de613e937c524b1a8c0999e
zahraabaydoun <zahraa.baydoun.88@gmail.com>
2017-02-08 19:38:22 +0200

    Add files via upload

---
+++ js/application.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $(document).on('init.slides', function() {
+    $('.loading-container').fadeOut(function() {
+      $(this).remove();
+    });
+  });
+
+  $('#slides').superslides({
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true,
+    hashchange: true,
+    scrollable: true
+  });
+
+  document.ontouchmove = function(e) {
+    e.preventDefault();
+  };
+  $('#slides').hammer().on('swipeleft', function() {
+    $(this).superslides('animate', 'next');
+  });
+
+  $('#slides').hammer().on('swiperight', function() {
+    $(this).superslides('animate', 'prev');
+  });
+});
